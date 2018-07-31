24f41919d435a9b5dbf3e4f811aaafc6d2df29ba
elbasir <elbasir@hotmail.com>
2018-07-31 13:45:41 +0300

    first commit

---
+++ Model.py
@@ -0,0 +1,92 @@
+import numpy as np
+np.random.seed(1337)
+from keras.models import load_model
+import pandas as pd
+import io
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing import sequence
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Input, Convolution1D, MaxPooling1D, Flatten
+from keras.layers.embeddings import Embedding
+from keras.layers.merge import Concatenate
+from keras.optimizers import Adam
+from keras.layers import  merge
+from keras.regularizers import l2
+from keras.layers import AveragePooling1D
+from sklearn.utils import shuffle
+from sklearn.utils import class_weight
+from keras.callbacks import ModelCheckpoint
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import pylab
+import itertools
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_auc_score
+from keras.models import model_from_json
+
+lr = 0.001 # Learning rate
+pl = 5 
+l2value = 0.001 # L2 regularization value
+stride_ = 1
+stride_max = 1
+border = 'same'
+
+main_input = Input(shape=(800,), dtype='int32', name='main_input')
+x = Embedding(output_dim=50, input_dim=22, input_length=800)(main_input)
+a = Convolution1D(64, 2, activation='relu', border_mode='same', W_regularizer=l2(l2value))(x)
+apool = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(a)
+b = Convolution1D(64, 3, activation='relu', border_mode='same', W_regularizer=l2(l2value))(x)
+bpool = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(b)
+c = Convolution1D(64, 8, activation='relu', border_mode='same', W_regularizer=l2(l2value))(x)
+cpool = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(c)
+d = Convolution1D(64, 9, activation='relu', border_mode='same', W_regularizer=l2(l2value))(x)
+dpool = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(d)
+f = Convolution1D(64, 4, activation='relu', border_mode='same', W_regularizer=l2(l2value))(x)
+fpool = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(f)
+g = Convolution1D(64, 5, activation='relu', border_mode='same', W_regularizer=l2(l2value))(x)
+gpool = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(g)
+h = Convolution1D(64, 6, activation='relu', border_mode='same', W_regularizer=l2(l2value))(x)
+hpool = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(h)
+i = Convolution1D(64, 7, activation='relu', border_mode='same', W_regularizer=l2(l2value))(x)
+ipool = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(i)
+merge2 = merge([apool, bpool, cpool, dpool,fpool,gpool,hpool, ipool], mode='concat', concat_axis=-1)
+merge2 = Dropout(0.3)(merge2)
+scalecnn1 = Convolution1D(64, 11, activation='relu', border_mode='same', W_regularizer=l2(l2value))(merge2)
+scale1 = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(scalecnn1)
+scalecnn2 = Convolution1D(64, 13, activation='relu', border_mode='same', W_regularizer=l2(l2value))(merge2)
+scale2 = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(scalecnn2)
+scalecnn3 = Convolution1D(64, 15, activation='relu', border_mode='same', W_regularizer=l2(l2value))(merge2)
+scale3 = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(scalecnn3)
+scale = merge([scale1, scale2, scale3], mode='concat', concat_axis=-1)
+scale = Dropout(0.3)(scale)
+cnn1 = Convolution1D(64, 5, activation='relu', border_mode='same', W_regularizer=l2(l2value))(scale)
+cnn10 = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(cnn1)
+cnn2 = Convolution1D(64, 9, activation='relu', border_mode='same', W_regularizer=l2(l2value))(scale)
+cnn20 = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(cnn2)
+cnn3 = Convolution1D(64, 13, activation='relu', border_mode='same', W_regularizer=l2(l2value))(scale)
+cnn30 = MaxPooling1D(pool_length=pl, stride=stride_max, border_mode=border)(cnn3)
+cnn50 = merge([cnn10, cnn20, cnn30], mode='concat', concat_axis=-1)
+cnn50 = Dropout(0.3)(cnn50)
+x = Flatten()(cnn50)
+x = Dense(256, activation='relu', name='FC', W_regularizer=l2(l2value))(x)
+output = Dense(1,activation='sigmoid', name='output', W_regularizer=l2(l2value))(x)
+model = Model(input=main_input, output=output)
+adam = Adam(lr=lr)
+model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+best_Weight_File="/name_of_the_weight_File.hdf5"
+checkpoint = ModelCheckpoint(best_Weight_File, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callback_list = [checkpoint]
+model.fit(X_train1, y_train1, validation_data=(X_validate, y_validate), class_weight=class_weight_dict, nb_epoch=300, batch_size=64, callbacks=callback_list)
+
+# Saving json and model files
+
+model_json = model.to_json()
+with open("/name_of_json_file.json", "w") as json_file:
+    json_file.write(model_json)
+
+model.save_weights("/name_of_model.h5")
+print("Saved model to disk")
+
---
+++ Test.py
@@ -0,0 +1,153 @@
+
+
+
+import numpy as np
+np.random.seed(1337)
+from keras.models import load_model
+import pandas as pd
+import io
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing import sequence
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Input, Convolution1D, MaxPooling1D, Flatten
+from keras.layers.embeddings import Embedding
+from keras.layers.merge import Concatenate
+from keras.optimizers import Adam
+from keras.layers import  merge
+from keras.regularizers import l2
+from keras.layers import AveragePooling1D
+from sklearn.utils import shuffle
+from sklearn.utils import class_weight
+from keras.callbacks import ModelCheckpoint
+from keras.models import model_from_json
+from Bio import SeqIO
+import argparse
+import csv
+
+def saveResults(Seq_id, predict):
+    fp = open('prediction_results.csv', 'w', newline='')
+    a = csv.writer(fp, delimiter=',')
+    elements = [['Sequence ID', 'Diffraction-quality Crystal Prediction']]
+    a.writerows(elements)
+
+
+
+    for i in range(0, len(Seq_id)):
+        a = csv.writer(fp, delimiter=',')
+        seq = Seq_id[i]
+        prediction = str(predict[i][10])
+        elements = [[seq, prediction]]
+        a.writerows(elements)
+    fp.close()
+    
+
+    
+def ensembleModel(embedTest, mean):
+    
+    for i in range(0, len(embedTest[:][:])):
+        sum = 0
+        for j in range(0, 11):
+            sum += mean[i][j]
+        
+        sum = sum/10.0
+        mean[i][10] = sum
+        
+    return(mean)
+
+
+def predictSeqs(embedTest, weights, jsonFiles):
+    mean = np.zeros((len(embedTest[:][:]), 11))
+    c = 0
+    adam = Adam(lr=0.001)
+    for i in range(0, len(weights)):
+        json_file = open(jsonFiles[i], 'r')
+        model_json = json_file.read()
+        json_file.close()
+        load_my_model = model_from_json(model_json)
+        load_my_model.load_weights(weights[i])
+        load_my_model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+        print("Prediction is in progress, please wait...")
+        score = load_my_model.predict(embedTest)
+        for i in range(0, len(score)):
+            mean[i][c] = score[i]
+        c = c + 1
+    
+    print("Prediction is done")
+    return (mean)
+
+def cleanSequenes(X_test):
+    amino_acids ='ACDEFGHIKLMNPQRSTVWXY'
+    for i in range(0, len(X_test)):
+        test = []
+        st = str(X_test[i])
+        testStr =''
+        for ch in st:
+            if(ch in amino_acids):
+                testStr += ch
+        
+        test.append(testStr)
+        X_test[i] = test
+
+    X = []
+    for i in range(len(X_test)):
+        if(len(X_test[i][0]) <= 800):
+            X.append(X_test[i][0])
+    X_test = np.array(X)
+    X_test = X_test.reshape(len(X_test),1)
+    
+    embedTest = []
+    for i in range(0, len(X_test)):
+        length = len(X_test[i][0])
+        pos = []
+        counter = 0
+        st = X_test[i][0]
+        for c in st:
+            AMINO_INDEX = amino_acids.index(c)
+            pos.append(AMINO_INDEX)
+            counter += 1
+        while(counter < 800):
+            pos.append(21)
+            counter += 1
+        embedTest.append(pos)
+            
+    return(embedTest)
+
+
+def parseFastaFile():
+    X_test = []
+    Seq_id = []
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=argparse.FileType('r'))
+    args = parser.parse_args()
+
+    with args.file as file:
+        fasta_sequences = SeqIO.parse(file,'fasta')
+        for fasta in fasta_sequences:
+            name, sequence = fasta.id, fasta.seq.tostring()
+            X_test.append(sequence)
+            Seq_id.append(name)
+            
+    return (X_test, Seq_id)
+
+def main():
+    
+    X_test = []
+    Seq_id = []
+    X_test, Seq_id = parseFastaFile()
+    embedTest = cleanSequenes(X_test)
+    
+    embedTest = np.array(embedTest)
+    mean = np.zeros((len(embedTest[:][:]), 11))
+    
+    weights = ['model1.hdf5', 'model2.hdf5', 'model3.hdf5', 'model4.hdf5', 'model5.hdf5',
+           'model6.hdf5', 'model7.hdf5', 'model8.hdf5', 'model9.hdf5', 'model10.hdf5']
+    jsonFiles = ['model1.json', 'model2.json', 'model3.json', 'model4.json', 'model5.json',
+             'model6.json', 'model7.json', 'model8.json', 'model9.json', 'model10.json']
+    
+    mean = predictSeqs(embedTest, weights, jsonFiles)
+    predict = ensembleModel(embedTest, mean)
+    saveResults(Seq_id, predict)
+    
+if __name__ == '__main__':
+    main()
+
